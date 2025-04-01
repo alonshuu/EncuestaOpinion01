403d7e7ec5e354c2902c8724e82de27a5bb3f079
Alonso Lagos <a.lagos10@ufromail.cl>
2025-04-01 14:19:12 -0300

    creado metodo crearmatriz y otrometodo para validar los numeros ingresados

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,12 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int M = obtenerNumeroValido(scan);
+        char[][] matris = crearMatriz(M);
+        llenarAprobaciones(matris, M);
+        mostrarResultados(matris, M);
+
 
     }
 
@@ -16,7 +22,7 @@ public class Main {
     // primer metodo solo se encarga de crear la matriz
 
     public static char[][] crearMatriz(int N) {
-        return new char[2][N];
+        return new char[2][N/2];
     }
 
     // el segundo metodo, lo implemente pensando en que sea capaz de validar el N que se ingrese
@@ -36,9 +42,48 @@ public class Main {
         } while (N <= 0);
 
         return N;
-        }
-    
+    }
 
+
+    // metodo para llenar con aprobaciones
+    public static void llenarAprobaciones(char[][] respuestas, int N) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("-- LLENAR CON SI --");
+        System.out.println("SELECCIONE EL GRUPO A EDITAR");
+        System.out.println("1. HOMBRES");
+        System.out.println("2. MUJERES");
+        int opcion = scanner.nextInt();
+
+        if (opcion != 1 && opcion != 2) {
+            System.out.println("Opción inválida.");
+            return;
+        }
+
+        int fila = (opcion == 1) ? 0 : 1;
+        int espacios = N;
+
+        System.out.println("Ingrese la cantidad de gente que aprueban la gestión");
+        int cantidad = scanner.nextInt();
+        int contador = 0;
+        for(int j = 0; j < respuestas[fila].length; j++){
+            respuestas[fila][j] = 's';
+            contador++;
+        }
+    }
+    public static void mostrarResultados(char[][] respuestas, int N){
+        System.out.println(" --- RESULTADOS ENCUESTA ------");
+        System.out.println("Hombres: ");
+        int contador = 0;
+        for (int i = 0; i < N/2 ; i++){
+            if(respuestas[0][i] == 's'){
+                contador++;
+            }
+
+
+        }
+        System.out.println(contador);
+    }
 
 
 }
